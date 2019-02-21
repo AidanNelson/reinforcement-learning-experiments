e16b6cfff556209e289e6649700936705c28e947
AidanNelson <AidanNelson@users.noreply.github.com>
2019-02-21 11:08:09 -0500

    added q-table-learning example

---
+++ q-learning-js/frozenLake.js
@@ -0,0 +1,194 @@
+
+
+class FrozenLake {
+  constructor(size, qAgent) {
+    this.size = size;
+    this.map = [];
+
+    this.qAgent = qAgent;
+    this.showTable = true;
+    
+    for (let i = 0; i < size; i++){
+      let row = [];
+      for (let j = 0; j < size; j++){
+        let next;
+        if (Math.random() < 0.25){
+          next = "H";
+        } else {
+          next = "F";
+        }
+        
+        if (i == 0 && j == 0){
+          next = "S";
+        }
+        if (i == size-1 && j == size-1){
+          next = "G";
+        }
+
+        row.push(next);
+      }
+      this.map.push(row);
+    }
+    // this.map =
+    //   [["S", "F", "F", "F"],
+    //   ["F", "H", "F", "H"],
+    //   ["F", "F", "F", "H"],
+    //   ["H", "F", "F", "G"]];
+
+    // agent begins at top left position
+    this.pos = { x: 0, y: 0 };
+
+  }
+
+  reset() {
+    //Reset the environment's state. Returns observation.
+    this.pos.x = 0; this.pos.y = 0;
+    // console.log(this.getCurrentState());
+    return this.getCurrentState();
+  }
+
+  step(action) {
+    // Step the environment by one timestep. Returns observation, reward, done
+
+    // move the player
+    if (action == 0) { // left
+      this.pos.x = Math.max(this.pos.x - 1, 0);
+    } else if (action == 1) { // down
+      this.pos.y = Math.min(this.pos.y + 1, this.size-1);
+    } else if (action == 2) { // right
+      this.pos.x = Math.min(this.pos.x + 1, this.size-1);
+    } else if (action == 3) { // up
+      this.pos.y = Math.max(this.pos.y - 1, 0);
+    }
+
+    let reward = 0;
+    let done = false;
+
+    if (this.map[this.pos.y][this.pos.x] == "G") {
+      reward = 1;
+      done = true;
+    }
+    if (this.map[this.pos.y][this.pos.x] == "H") {
+      done = true;
+    }
+
+    return {
+      nextState: this.getCurrentState(),
+      reward: reward,
+      done: done
+    }
+  }
+
+  getMaxRewardFromCurrentState() {
+    let maxReward = 0;
+
+    if (this.map[this.pos.y][this.pos.x] == "G") {
+      reward = 1;
+      done = true;
+    }
+    if (this.map[this.pos.y][this.pos.x] == "H") {
+      done = true;
+    }
+  }
+
+  render() {
+    // reset background
+    background(200, 100, 200);
+
+    let side = width > height ? height : width;
+
+    // render one frame of the environment.
+    let buff = side / 6;
+    let spaceSize = (side - buff) / this.size;
+
+
+
+    for (let x = 0; x < this.map[0].length; x++) {
+      for (let y = 0; y < this.map.length; y++) {
+
+        
+        let em = "";
+        switch (this.map[y][x]) {
+          case "S":
+            // em = "🔸";
+            break;
+          case "F":
+            // em = "🔸";
+            break;
+          case "H":
+            em = "☠";
+            break;
+          case "G":
+            em = "🎉";
+            break;
+        }
+        if (this.showTable){
+          let stateIndex = (y * this.map[0].length) + x;
+          let actionArr = this.qAgent.qt[stateIndex];
+          let actionIndex = -1;
+          let maxSoFar = -1000;
+        
+          for (let p = 0; p < actionArr.length; p++){
+
+            if (actionArr[p] > maxSoFar) {
+              actionIndex = p;
+              maxSoFar = actionArr[p];
+            }
+          }
+          // console.log('action at x = ',x, ' / y = ', y, ' --> ',actionIndex);
+          let arrow = '';
+          switch (actionIndex) {
+            case 0:
+              arrow = "⬅";
+              break;
+            case 1:
+              arrow = "⬇";
+              break;
+            case 2:
+              arrow = "➡";
+              break;
+            case 3:
+              arrow = "⬆";
+              break;
+          }
+          textSize(64);
+          stroke(0);
+          fill(200,200,200,200);
+          textAlign(CENTER);
+          text(arrow, buff + x * spaceSize, (buff + y * spaceSize));
+        }
+
+        textSize(64);
+        textAlign(CENTER);
+        text(em, buff + x * spaceSize, (buff + y * spaceSize));
+      }
+    }
+
+    background(255,255,255,50);
+    let em = "";
+    // render player position
+    switch (this.map[this.pos.y][this.pos.x]) {
+      case "S":
+        em = '😐';
+        break;
+      case "F":
+        em = '😐';
+        break;
+      case "H":
+        em = '😱';
+        break;
+      case "G":
+        em = '🤩';
+        break;
+    }
+
+    text(em, this.pos.x * spaceSize + buff, this.pos.y * spaceSize + buff);
+    // fill(200, 0, 0, 50);
+    // ellipse(this.pos.x * spaceSize + buff, this.pos.y * spaceSize + buff, 50);
+  }
+
+  getCurrentState() {
+    // return a number from 0-15 for current state of player
+    return this.pos.y * this.size + this.pos.x;
+  }
+}
---
+++ q-learning-js/sketch.js
@@ -0,0 +1,38 @@
+/*
+A series of Q-Learning experiments built in javascript using tensorflow.js and p5.js
+Aidan Nelson, Spring 2019
+
+References:
+Arthur Juliani Series on Q Learning: 
+  https://medium.com/emergent-future/simple-reinforcement-learning-with-tensorflow-part-0-q-learning-with-tables-and-neural-networks-d195264329d0
+  https://gist.github.com/awjuliani/9024166ca08c489a60994e529484f7fe#file-q-table-learning-clean-ipynb
+
+Environment based on OpenAI Gym 'frozen lake' environment:
+  https://gym.openai.com/envs/FrozenLake-v0/
+  https://github.com/openai/gym
+
+Dan Shiffman on tensorflow.js Layers API: 
+  https://www.youtube.com/watch?v=F4WWukTWoXY&index=7&list=PLRqwX-V7Uu6YIeVA3dNxbR9PYj4wV31oQ
+
+*/
+let qAgent;
+
+
+function setup(){
+    createCanvas(800,800);
+    background(200,100,200);
+
+    
+    qAgent = new QAgent(7);
+    // qAgent = new QNetwork(4);
+
+    let trainButton = createButton('Train Q Agent');
+    trainButton.mousePressed(() =>{qAgent.train(100000)});
+
+    let runButton = createButton('Run Q Agent');
+    runButton.mousePressed(() =>{qAgent.stepThrough()});
+  }
+  
+  
+  function draw(){
+  }
